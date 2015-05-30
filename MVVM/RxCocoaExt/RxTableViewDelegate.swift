d18b52dd9a65839a1237ee02efbc8a15c75386b9
Daniel Tartaglia <dtartaglia@hanekedesign.com>
2015-05-30 14:55:53 -0400

    Added more observers to RxTableViewDelegate for future use.

--- MVVM/RxCocoaExt/RxTableViewDelegate.swift
+++ MVVM/RxCocoaExt/RxTableViewDelegate.swift
@@ -26,6 +26,18 @@ class RxTableViewDelegate: NSObject, UITableViewDelegate {
 		return _willDisplayFooterViewForSection
 	}
 
+	var rx_didEndDisplayingCellForRowAtIndexPath: Observable<(cell: UITableViewCell, indexPath: NSIndexPath)> {
+		return _didEndDisplayingCellForRowAtIndexPath
+	}
+	
+	var rx_didEndDisplayingHeaderViewForSection: Observable<(view: UIView, section: Int)> {
+		return _didEndDisplayingHeaderViewForSection
+	}
+	
+	var rx__didEndDisplayingFooterViewForSection: Observable<(view: UIView, section: Int)> {
+		return _didEndDisplayingFooterViewForSection
+	}
+	
 	// Selection
 	var rx_didSelectRowAtIndexPath: Observable<NSIndexPath> {
 		return _didSelectRowAtIndexPath
@@ -36,6 +48,22 @@ class RxTableViewDelegate: NSObject, UITableViewDelegate {
 		sendNext(_willDisplayCellForRowAtIndexPath, (cell: cell, indexPath: indexPath))
 	}
 
+	func tableView(tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+		sendNext(_willDisplayHeaderViewForSection, (view: view, section: section))
+	}
+	
+	func tableView(tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
+		sendNext(_willDisplayFooterViewForSection, (view: view, section: section))
+	}
+	
+	func tableView(tableView: UITableView, didEndDisplayingHeaderView view: UIView, forSection section: Int) {
+		sendNext(_didEndDisplayingHeaderViewForSection, (view: view, section: section))
+	}
+	
+	func tableView(tableView: UITableView, didEndDisplayingFooterView view: UIView, forSection section: Int) {
+		sendNext(_didEndDisplayingFooterViewForSection, (view: view, section: section))
+	}
+	
 	func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
 		sendNext(_didSelectRowAtIndexPath, indexPath)
 	}
@@ -43,6 +71,9 @@ class RxTableViewDelegate: NSObject, UITableViewDelegate {
 	private let _willDisplayCellForRowAtIndexPath: Subject<(cell: UITableViewCell, indexPath: NSIndexPath)> = Subject()
 	private let _willDisplayHeaderViewForSection: Subject<(view: UIView, section: Int)> = Subject()
 	private let _willDisplayFooterViewForSection: Subject<(view: UIView, section: Int)> = Subject()
+	private let _didEndDisplayingCellForRowAtIndexPath: Subject<(cell: UITableViewCell, indexPath: NSIndexPath)> = Subject()
+	private let _didEndDisplayingHeaderViewForSection: Subject<(view: UIView, section: Int)> = Subject()
+	private let _didEndDisplayingFooterViewForSection: Subject<(view: UIView, section: Int)> = Subject()
 	private let _didSelectRowAtIndexPath: Subject<NSIndexPath> = Subject()
 	
 }
